c36b20f9d85028120d14b57186f7f6eb9b25b946
mervezz096 <mervezz096@gmail.com>
2023-07-09 20:28:04 +0300

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:biyemek/firebase_options.dart';
 import 'package:biyemek/screens/business_screens/home/business_home_page.dart';
+import 'package:biyemek/screens/onboarding/onboarding_screen.dart';
 import 'package:biyemek/services/already_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +28,44 @@ class MyApp extends StatelessWidget {
       },
       debugShowCheckedModeBanner: false,
       title: "Bi'Yemek",
-      home: const AuthPage(),
+      home: const SplashScreen(),
     );
+  }
+}
+ 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+
+        splash: Column(
+
+         children: [
+           Image.asset("assets/images/onboarding/intro1.png"),
+
+          SizedBox(height: 30),
+          SpinKitThreeInOut(
+           color: Colors.red,
+           size: 50.0,
+
+
+          )
+
+         ],
+        ),
+        backgroundColor: Colors.greenAccent,
+        nextScreen: AuthPage(),
+    splashIconSize: 500,
+      duration: 4000,
+      splashTransition:SplashTransition.slideTransition ,
+    );
+
   }
 }
