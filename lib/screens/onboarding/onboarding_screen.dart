64a9b457125538e1eea713e38cd0656e831a25f0
Huseyin Asan <asanhuseyin24@gmail.com>
2023-07-17 05:08:33 +0300

    onboarding screens updated

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:biyemek/constants/colors.dart';
 import 'package:biyemek/screens/onboarding/entrances/choose_entrance.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
@@ -32,59 +33,121 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
                 ? Container()
                 : const Icon(Icons.arrow_back_ios_new_rounded),
             pages: [
+              //first page
               Container(
                 color: Colors.greenAccent,
                 child: Column(
                   children: [
                     const SizedBox(height: 100),
                     Image.asset("assets/images/onboarding/intro1.png"),
+                    Text(
+                      "Bi'Yemek",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.clip,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      """İsrafı önlemenin en ucuz ve taze hali""",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                      overflow: TextOverflow.clip,
+                    ),
                   ],
                 ),
               ),
+              //second page
               Container(
                 color: Colors.pinkAccent,
                 child: Column(
                   children: [
                     const SizedBox(height: 50),
                     Image.asset("assets/images/onboarding/intro2.png"),
+                    Text(
+                      "İşletmeler",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.clip,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Text(
+                          "Gıda ürünlerini listeleyelerek hem gıda israfını hem de mali zararı önleyebilir!",
+                          style: TextStyle(
+                            fontSize: 20,
+                            overflow: TextOverflow.clip,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
+              //third page
               Container(
                 color: Colors.yellow,
                 child: Column(
                   children: [
                     const SizedBox(height: 50),
                     Image.asset("assets/images/onboarding/intro3.png"),
-                    const SizedBox(height: 200),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const ChooseEntrance();
-                            },
+                    SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          "Gıda ürünlerini çok uygun fiyatlara satın al ve israfın önüne geç!",
+                          style: TextStyle(
+                            fontSize: 20,
+                            overflow: TextOverflow.clip,
+                            color: Colors.black,
                           ),
-                        );
-                      },
-                      child: Expanded(
-                        child: Container(
-                          width: 300,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(
-                              color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 50),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const ChooseEntrance();
+                              },
                             ),
-                          ),
-                          child: const Center(
-                            child: Text(
-                              "Başla",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                          );
+                        },
+                        child: Expanded(
+                          child: Container(
+                            width: 300,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: AppColors.tertiaryColor,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
                                 color: Colors.white,
+                              ),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Başla",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
